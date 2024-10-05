bcfa6d8a5b131df4d6877af3a4baddc616341e65
Kaviyarasu <101913405+kaviyarasu26@users.noreply.github.com>
2024-10-05 14:54:24 +0530

    commit iframe

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -3,7 +3,8 @@ import "../styleSheet/game.css";
 const Game =()=>{
     return(
         <div className="game-working-area">
-            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQAJ5TnGIKsA1HKr19cRGNvgKa0GQHg3ZkzscCeNRUHUE3jnhzCungHCSITZvGzaDTnoECqq-ton9vo/embed?start=false&loop=false&delayms=3000&rm=minimal" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" ></iframe>
+            {/* <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQAJ5TnGIKsA1HKr19cRGNvgKa0GQHg3ZkzscCeNRUHUE3jnhzCungHCSITZvGzaDTnoECqq-ton9vo/embed?start=false&loop=false&delayms=3000&rm=minimal" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" ></iframe> */}
+            <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRyRoth39wmXqB-Twc54ZuqlTgwgDCaOQZ5aUGv02l30cXHrKz7XPSkTDrdBUsWJ59DjDSqFuLGhF8X/embed?start=false&loop=false&delayms=3000&rm=minimal" frameborder="0" width="1100" height="652" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
         </div>
     );
 }
